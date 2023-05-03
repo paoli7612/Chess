3ef3b2dad378d985cee0655c0cde521b2696de86
paoli7612 <pali7612@gmail.com>
2023-05-03 14:40:48 +0200

    fix img king

--- pieces/king.lua
+++ pieces/king.lua
@@ -4,9 +4,9 @@ function King(boss, x, y, team)
     local king = Piece(boss, x, y, team)
 
     if team then
-        king.quad = love.graphics.newQuad(128*3, 128, 128, 128, image:getDimensions())
+        king.quad = love.graphics.newQuad(128*4, 128, 128, 128, image:getDimensions())
     else
-        king.quad = love.graphics.newQuad(128*3, 0, 128, 128, image:getDimensions())
+        king.quad = love.graphics.newQuad(128*4, 0, 128, 128, image:getDimensions())
     end
     return king
 end
